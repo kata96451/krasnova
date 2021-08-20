702d77f46f4bffffb6e3d56452e09de52f494dff
Katrin <filmprof34@gmail.com>
2021-08-20 22:10:33 +0300

    dz 29/07/with postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 var sass = require('gulp-sass')(require('sass'));
 
 sass.compiler = require('node-sass');
@@ -8,6 +11,10 @@ sass.compiler = require('node-sass');
 gulp.task('scss', function () {
     return gulp.src('./src/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer({browsers: ['last 1 version']}),
+            cssnano()
+        ]))
         .pipe(gulp.dest('./dist'));
 });
 
