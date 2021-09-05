d14fcc8c9d4a13c945961adefec7497bde5d6746
Katrin <filmprof34@gmail.com>
2021-09-05 15:09:53 +0300

    dz 31/08/1

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ const image = require('gulp-image');
 
 sass.compiler = require('node-sass');
 
-gulp.task('scss', function () {
-    return gulp.src('./src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([
-            autoprefixer({browsers: ['last 1 version']}),
-            cssnano()
-        ]))
-        .pipe(gulp.dest('./dist'));
-});
+// gulp.task('scss', function () {
+//     return gulp.src('./src/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(postcss([
+//             autoprefixer({browsers: ['last 1 version']}),
+//             cssnano()
+//         ]))
+//         .pipe(gulp.dest('./dist'));
+// });
 
 gulp.task('scss:watch', function () {
     gulp.watch('./src/**/*.scss',  gulp.series(['scss']));});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/js/index/js",
+    entry: {
+        index: [
+            path.resolve(__dirname, "src/js/index.js"),
+            path.resolve(__dirname, "src/style.scss")
+            ]
+    },
     output: {
         path: path.resolve(__dirname, "dist/js"),
         filename: "main.min.js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: '../css/[name].css'
+    })],
 }
